3f47c1e7c31407168fdbf2efdb76da37ade227f4
I Komang Cleon Kalea <99885537+CleonKalea@users.noreply.github.com>
2022-05-12 07:04:08 +0800

    5_Encrypt Decrypt

--- Modul 3/5_Encrypt Decript.c
+++ Modul 3/5_Encrypt Decript.c
@@ -1,17 +1,13 @@
-// I Komang Cleon Kalea
-// 2105551090
-// Kelompok 18
-// Modul 3 no.5
-
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
 
 void clear(){
     system("cls || clear");
 }
 
 void pembatas(){
-    char pembatas[] = "---------------------------------------------------------\n";
+    char pembatas[] = "----------------------------------------------------------------------------\n";
     printf("%s", pembatas);
 }
 
@@ -35,19 +31,123 @@ int validasi_integer() {
     }
 }
 
+void hasil_enkripsi_cypher(int cypher){
+    char alphabets[26];
+    char pergeseran_alphabets[26];
+    char huruf;
+    int i = 0;
+
+    pembatas();
+
+    for(huruf = 'A'; huruf <= 'Z' ; huruf++){
+        if(i < 26){
+            alphabets[i] = huruf;
+            i++;
+        }
+    }
+
+    for(i = 0 ; i < 26 ; i++){
+        printf("%c  ", alphabets[i]);
+    }
+
+    printf("\n");
+
+    for(i = 0 ; i < 26 ; i++){
+        printf("|  ");
+    }
+
+    printf("\n");
+
+    strcpy(pergeseran_alphabets, alphabets);
+    
+    for(i = 0; pergeseran_alphabets[i] != '\0'; i++){
+        if(pergeseran_alphabets[i] >= 'A' && pergeseran_alphabets[i] <= 'Z') {
+            pergeseran_alphabets[i] = pergeseran_alphabets[i] + 32;
+        }
+        
+        if(pergeseran_alphabets[i] >= 'a' && pergeseran_alphabets[i] <= 'z'){
+            pergeseran_alphabets[i] = pergeseran_alphabets[i] + cypher;
+
+            if(pergeseran_alphabets[i] > 'z'){
+                pergeseran_alphabets[i] = pergeseran_alphabets[i] - 26;
+            }
+            pergeseran_alphabets[i] = pergeseran_alphabets[i] - 32;
+        }
+	}
+
+    for(i = 0 ; i < 26 ; i++){
+        printf("%c  ", pergeseran_alphabets[i]);
+    }
+
+    printf("\n");
+}
+
+void hasil_dekripsi_cypher(int cypher){
+    char alphabets[26];
+    char pergeseran_alphabets[26];
+    char huruf;
+    int i = 0;
+
+    pembatas();
+
+    for(huruf = 'A'; huruf <= 'Z' ; huruf++){
+        if(i < 26){
+            alphabets[i] = huruf;
+            i++;
+        }
+    }
+
+    for(i = 0 ; i < 26 ; i++){
+        printf("%c  ", alphabets[i]);
+    }
+
+    printf("\n");
+
+    for(i = 0 ; i < 26 ; i++){
+        printf("|  ");
+    }
+
+    printf("\n");
+
+    strcpy(pergeseran_alphabets, alphabets);
+    
+    for(i = 0; pergeseran_alphabets[i] != '\0'; i++){
+		if(pergeseran_alphabets[i] >= 'a' && pergeseran_alphabets[i] <= 'z') {
+        	pergeseran_alphabets[i] = pergeseran_alphabets[i] - 32;
+      	}
+      
+		if(pergeseran_alphabets[i] >= 'A' && pergeseran_alphabets[i] <= 'Z'){
+			pergeseran_alphabets[i] = pergeseran_alphabets[i] - cypher;
+
+			if(pergeseran_alphabets[i] < 'A'){
+				pergeseran_alphabets[i] = pergeseran_alphabets[i] + 26;
+			}
+			pergeseran_alphabets[i] = pergeseran_alphabets[i] +32;
+		}
+	}
+
+    for(i = 0 ; i < 26 ; i++){
+        printf("%c  ", pergeseran_alphabets[i]);
+    }
+    
+    printf("\n");
+}
+
 void enkripsi(){
     char text[255];
-    char huruf;
-    int i;
+    int i = 0;
     int cypher;
 
+    pembatas();
     printf("Masukkan Kalimat yang ingin dienkripsi : ");
     scanf(" %[^\n]", text);
 
     printf("Masukkan Cipherkey : ");
     cypher = validasi_integer();
 
-    for(i = 0; text[i] != '\0'; ++i){
+    hasil_enkripsi_cypher(cypher);
+    
+    for(i = 0; text[i] != '\0'; i++){
 		if(text[i] >= 'A' && text[i] <= 'Z') {
         	text[i] = text[i] + 32;
       	}
@@ -57,27 +157,29 @@ void enkripsi(){
 			if(text[i] > 'z'){
 				text[i] = text[i] - 26;
 			}
-			text[i] = text[i] -32;
+			text[i] = text[i] - 32;
 		}
 	}
 
     pembatas();
-    printf("Hasil Enkripsi : %s", text);
+    printf("Hasil Enkripsi : %s\n", text);
 }
 
 void dekripsi(){
     char text[255];
-    char huruf;
     int i;
     int cypher;
 
+    pembatas();
     printf("Masukkan Kalimat yang ingin didekripsi : ");
     scanf(" %[^\n]s", text);
 
     printf("Masukkan Cipherkey : ");
     cypher = validasi_integer();
 
-    for(i = 0; text[i] != '\0'; ++i){
+    hasil_dekripsi_cypher(cypher);
+
+    for(i = 0; text[i] != '\0'; i++){
 		if(text[i] >= 'a' && text[i] <= 'z') {
         	text[i] = text[i] - 32;
       	}
@@ -87,12 +189,12 @@ void dekripsi(){
 			if(text[i] < 'A'){
 				text[i] = text[i] + 26;
 			}
-			text[i] = text[i] +32;
+			text[i] = text[i] + 32;
 		}
 	}
 
     pembatas();
-    printf("Hasil Dekripsi : %s", text);
+    printf("Hasil Dekripsi : %s\n", text);
 }
 
 int main(){
@@ -103,12 +205,13 @@ int main(){
     do{
         clear();
         j = 0; 
+
         do{
      
-            printf("\t\tENKRIPSI DAN DEKRIPSI\n");
+            printf("\t\t\t    ENKRIPSI DAN DEKRIPSI\n");
             pembatas();
 
-            printf("1. Enkripsi\n2. Dekripsi\n");
+            printf("1. Enkripsi\n2. Dekripsi\n3. Keluar Dari Program\n");
             pembatas();
 
             printf("Masukkan Program Yang Ingin Digunakan : ");
@@ -116,16 +219,21 @@ int main(){
             
             pembatas();
             if(pilihan == 1){
+                clear();
                 enkripsi();
                 j = 1;
 
             }else if(pilihan == 2){
+                clear();
                 dekripsi();
                 j = 1;
 
+            }else if(pilihan == 3){
+                clear();
+                exit(1);
+
             }else{
                 clear();
-
                 pembatas();
                 printf("%d <- Input yang dimasukkan tidak valid !!\n\n", pilihan);
                 pembatas();
